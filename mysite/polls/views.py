91174635adaa47d6a57b5604c8c97e74c5bf3df9
Brett Andrews <brettandrews3@gmail.com>
2021-08-27 17:01:48 -0500

    Filled in the question template on ~/polls/detail.html. Tested and working.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
+#from django.http import HttpResponse      (Now handled by importing get_object_or_404)
+#from django.http import Http404
+from django.shortcuts import get_object_or_404
 from .models import Question
 # Create your views here.
 
@@ -10,13 +11,11 @@ def index(request):
 	context = {'latest_question_list': latest_question_list}
 	return render(request, 'polls/index.html', context)
 
+# This detail() uses get_object_or_404() to maintain loose coupling,
+# whereas using the detail() shown below ties the model layer to the view layer.
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("This question doesn't exist.")
+	question = get_object_or_404(Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
-	#return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
@@ -24,3 +23,15 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
+
+
+"""
+# This is the long-form def detail(). The production one above uses shortcuts.
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("This question doesn't exist.")
+	return render(request, 'polls/detail.html', {'question': question})
+	#return HttpResponse("You're looking at question %s." % question_id)
+"""
