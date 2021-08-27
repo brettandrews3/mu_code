a3314d7c31696ec0d913381b006a2cacd868617a
Brett Andrews <brettandrews3@gmail.com>
2021-08-27 16:43:04 -0500

    Added 404 error exception in polls/views.py. This exception pops if a question no. doesn't exist.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,21 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import Question
 # Create your views here.
 
-"""
-# Here's the original def index(). The revised one is simplified.
-from django.template import loader
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = {
-	    'latest_question_list': latest_question_list,
-	}
-	#output = ', '.join([q.question_text for q in latest_question_list])
-	#return HttpResponse(output)
-	return HttpResponse(template.render(context, request))
-"""
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -23,7 +11,12 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("This question doesn't exist.")
+	return render(request, 'polls/detail.html', {'question': question})
+	#return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
