866aaf84f6cacaa73870b0efe6210440fa9ad9fb
Brett Andrews <brettandrews3@gmail.com>
2021-08-30 17:58:15 -0500

    I've got this error that I cannot figure out. May back up to last commit. Message: File /home/pi/mu_code/mysite/polls/urls.py, line 8, in <module>     path('', views.IndexView.as_view(), name='index'), AttributeError: module 'polls.views' has no attribute 'IndexView'

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,22 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
+
+# Using Django's generic views to keep URL changes simple, loosely coupled:
+urlpatterns = [
+	path('', views.IndexView.as_view(), name='index'),
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+	path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+	path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
+
+
+
+
+
+"""Here's the original, longform setup for URLs. Compare to production setup above.
 urlpatterns = [
 	# ex: /polls/
 	path('', views.index, name='index'),
@@ -12,3 +28,4 @@ urlpatterns = [
 	# ex: /polls/5/vote/
 	path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+"""
